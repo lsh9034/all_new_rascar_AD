e8eb302a741a0847b74af77245395bd8f929289e
Sihyun Lee <lsh9034@kookmin.ac.kr>
2018-11-29 18:15:27 +0900

    asdf

--- lee_Ad_assignment_main.py
+++ lee_Ad_assignment_main.py
@@ -121,7 +121,7 @@ class myCar(object):
         self.move(speed)
         self.turn(30)
         time.sleep(1.5)
-        self.turn(-15)
+        self.turn(-20)
         self.move(-(speed))
         time.sleep(1)
         while(not self.car.line_detector.is_in_line()):
