e6649b5433e7735707dbfe908e4876c57a270348
Sihyun Lee <lsh9034@kookmin.ac.kr>
2018-11-29 17:18:16 +0900

    asdf

--- lee_Ad_assignment_main.py
+++ lee_Ad_assignment_main.py
@@ -132,7 +132,7 @@ class myCar(object):
         past_degree = 0
         check_Start = True
         start_time = time.time()
-        while(time.time() - start_time >= 1.5):
+        while(time.time() - start_time <= 1.5):
             lines = self.read_digit()
             check,degree = self.compute_degree(lines)
             if(past_degree != degree):
